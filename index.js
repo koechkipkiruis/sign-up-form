4444bc80b7da106c09a0b41fde8d11770dce5131
Shadrack Koech <koechkipkiruis@gmail.com>
2023-01-10 10:33:18 +0300

    commit initial files

---
+++ index.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import ReeactDOM from 'react-dom'
+import App from "./App"
+
+ReeactDOM.render(<App />, document.getElementById("root"))
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(<h1>Hello, world!</h1>);
